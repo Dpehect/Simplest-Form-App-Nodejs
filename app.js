a6e2dff422f41bd537d833c3602095691361f773
Yunus Emre Gürlek <105203804+Dpehect@users.noreply.github.com>
2022-12-25 18:09:23 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+const url = 'mongodb+srv://Dpehect:yunusemre366@cluster0.olfqcop.mongodb.net/test';
+const dbName = 'mydb';
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/save', (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    const db = client.db(dbName);
+    const collection = db.collection('users');
+    collection.insertOne({ name: name, age: age }, (err, result) => {
+      res.redirect('/list');
+      client.close();
+    });
+  });
+});
+
+app.get('/list', (req, res) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    const db = client.db(dbName);
+    const collection = db.collection('users');
+    collection.find({}).toArray((err, docs) => {
+      res.render('list', { users: docs });
+      client.close();
+    });
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
